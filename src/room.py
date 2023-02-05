89fbeee984a5b5f66c6f8e36529ccef61be4f435
Shazreen Seatter <shazseatter12@gmail.com>
2023-02-05 17:35:33 +0000

    test passed room has price

--- src/room.py
+++ src/room.py
@@ -1,7 +1,6 @@
 
 class Room: 
-    def __init__(self, guests, price):
-        self.guests = guests
+    def __init__(self, price):
         self.price = price 
         self.songs = []
         self.guests = []
--- tests/room_test.py
+++ tests/room_test.py
@@ -1,2 +1,12 @@
 import unittest
 from src.room import Room
+
+
+class TestRoom(unittest.TestCase):
+    def setUp(self):
+        self.room = Room(20)
+
+    def test_price_of_room(self):
+        expected = 20
+        actual = self.room.price
+        self.assertEqual(expected, actual)

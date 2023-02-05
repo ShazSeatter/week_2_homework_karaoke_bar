64d8e3dc0bd24877f6917ccabbfabf25a964b83a
Shazreen Seatter <shazseatter12@gmail.com>
2023-02-05 17:14:49 +0000

    test pass for guest_has_name

--- tests/guest_test.py
+++ tests/guest_test.py
@@ -1,2 +1,11 @@
 import unittest
 from src.guest import Guest
+
+class TestGuest(unittest.TestCase):
+    def setUp(self):
+        self.guest = Guest("John Smith", 50)
+
+    def test_guest_has_name(self):
+        expected = "John Smith"
+        actual = self.guest.name
+        self.assertEqual(expected, actual)

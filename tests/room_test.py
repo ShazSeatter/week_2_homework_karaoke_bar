c2569c1341a167cf0185b2fdfc0bcea2695d9421
Shazreen Seatter <shazseatter12@gmail.com>
2023-02-05 17:44:18 +0000

    test passed for has songs and guests for Room test

--- tests/room_test.py
+++ tests/room_test.py
@@ -10,3 +10,17 @@ class TestRoom(unittest.TestCase):
         expected = 20
         actual = self.room.price
         self.assertEqual(expected, actual)
+
+    def test_has_songs(self):
+        expected = []
+        actual = self.room.songs
+        self.assertEqual(expected, actual)
+
+    def test_has_guests(self):
+        expected = []
+        actual = self.room.guests
+        self.assertEqual(expected, actual)
+
+
+    # def test_add_songs_to_room(self):
+    #     self.song = Song()

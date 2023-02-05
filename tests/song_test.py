b154397a450d49881f4aa1e64b751f0752ab0be2
Shazreen Seatter <shazseatter12@gmail.com>
2023-02-05 17:20:46 +0000

    test passed for song has artist

--- tests/song_test.py
+++ tests/song_test.py
@@ -10,3 +10,8 @@ class TestSong(unittest.TestCase):
         expected = "Hound Dog"
         actual = self.song.title
         self.assertEqual(expected, actual)
+
+    def test_song_has_artist(self):
+        expected = "Elvis Presley"
+        actual = self.song.artist
+        self.assertEqual(expected, actual)
